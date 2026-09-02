ecd534e2378a8e9738350714720c8619e33e0703
MohammadAli0096 <mohammadaliattari92@gmail.com>
2026-09-02 14:28:52 +0500

    print aal names

---
+++ main.dart
@@ -0,0 +1,18 @@
+//Q.1: Create a list of names and print all names using list.
+void main() {
+  List names = [
+    "ali",
+    "ahmed",
+    "hassan",
+    "hussain",
+    "bilal",
+    "umer",
+    "saad",
+    "hamza",
+    "junaid",
+    "shahzaib",
+  ];
+  for (String name in names) {
+    print(name);
+  }
+}
