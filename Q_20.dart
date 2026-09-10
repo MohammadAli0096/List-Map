0c4e55b3a9d3617d288e9d64fff236d05e7aaaa9
MohammadAli0096 <mohammadaliattari92@gmail.com>
2026-09-10 14:19:29 +0500

    use && operator

---
+++ Q_20.dart
@@ -0,0 +1,14 @@
+// Q.20: Create a map named "car" with the following key-value pairs: "brand" as "Toyota", "color" as "Red", "isSedan" as true. Write Dart code to check if the car is a sedan and red in color. Print "Match" if both conditions are true, otherwise print "No match".
+void main() {
+  Map<String, dynamic> car = {
+    "BRAND": "Toyota",
+    "COLOR": "Red",
+    "Is SEDAN": true,
+  };
+
+  if (car["Is SEDAN"] == true && car["COLOR"] == "Red") {
+    print("MATCH");
+  } else {
+    print("NO MATCH");
+  }
+}
